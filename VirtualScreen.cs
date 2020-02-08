a26d5f259444cb03db1a2232b80b69d505123edb
Benjamin McLean <mclean.ben@gmail.com>
2020-02-08 15:54:50 -0600

    Added Viewport and Target

--- VirtualScreen.cs
+++ VirtualScreen.cs
@@ -10,9 +10,30 @@ public class VirtualScreen : StaticBody
     public const float Height = 2.7f;
     public CollisionShape CollisionShape { get; set; }
     public MeshInstance MeshInstance { get; set; }
+    private Viewport Viewport;
+    private ColorRect Target;
 
     public VirtualScreen()
     {
+        AddChild(Viewport = new Viewport()
+        {
+            Size = new Vector2(64, 64),
+            Disable3d = true,
+            RenderTargetClearMode = Viewport.ClearMode.OnlyNextFrame,
+            RenderTargetVFlip = true,
+        });
+        Viewport.AddChild(new Sprite()
+        {
+            Centered = false,
+            Texture = (Texture)GD.Load("res://icon.png"),
+            Scale = new Vector2(2f / 3f, 2f / 3f),
+        });
+        Viewport.AddChild(Target = new ColorRect()
+        {
+            Color = Color.Color8(0, 0, 255, 255),
+            RectSize = new Vector2(2, 2),
+            //RectPosition = new Vector2(Viewport.Size.x / 2f, Viewport.Size.y / 2f),
+        });
         MeshInstance = new MeshInstance()
         {
             Mesh = new QuadMesh()
@@ -21,7 +42,8 @@ public class VirtualScreen : StaticBody
             },
             MaterialOverride = new SpatialMaterial()
             {
-                AlbedoTexture = (Texture)GD.Load("res://icon.png"),
+                AlbedoTexture = Viewport.GetTexture(),
+                //AlbedoTexture = (Texture)GD.Load("res://icon.png"),
                 FlagsUnshaded = true,
                 FlagsDoNotReceiveShadows = true,
                 FlagsDisableAmbientLight = true,
@@ -44,5 +66,12 @@ public class VirtualScreen : StaticBody
         });
         CollisionShape.AddChild(MeshInstance);
 
+        TargetPosition = new Vector2(Height / 2f, Height / 2f);
+    }
+
+    public Vector2 TargetPosition
+    {
+        set => Target.RectPosition = new Vector2(value.x / Height * Viewport.Size.x, value.y / Height * Viewport.Size.y);
+        get => new Vector2(Target.RectPosition.x / Viewport.Size.x * Height, Target.RectPosition.y / Viewport.Size.y * Height);
     }
 }
